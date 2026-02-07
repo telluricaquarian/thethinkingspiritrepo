303c25b5537946bc6d0c9b6accb7b20c69351b6c
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2026-02-06 20:07:36 -0800

    Adjust mobile image handling for Full-Stack Build card

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function Page() {
                 <ProductCard
                   accent="orange"
                   imageUrl="/images/onitos.png"
-                  imageUrlMobile="/images/images/Internal%20O.S.png"
+                  imageUrlMobile="/images/images/Internal O.S.png"
                   rightImageUrlDesktop="/images/Internal%20O.S.png"
                   title="Full-Stack Build"
                   specifications={[
